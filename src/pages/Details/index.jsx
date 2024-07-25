05b9bc44dffe15f66145eaa80b95236c1c991d9d
Yara Daudt <yaracrisalis@gmail.com>
2024-07-25 16:40:45 +0100

    Creating project's base

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,12 @@
+// import { useState } from 'react'
+import { Container } from './styles.js'
+
+export function Details() {
+  return (
+    <Container>
+    <h1>Hello World!</h1>
+    <p>laLALAL</p>
+    </Container>
+  )
+}
+

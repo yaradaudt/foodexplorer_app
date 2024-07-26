0939dcec706ba6079eea5097ab07ce4230ff806f
Yara Daudt <yaracrisalis@gmail.com>
2024-07-26 17:12:23 +0100

    Global style inserted

---
+++ src/styles/global.js
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from "styled-components"
+
+export default createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        background-color: ${({ theme }) => theme.COLORS.BACKGROUND_100};
+        color: ${({ theme }) => theme.COLORS.LIGHT_100};
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    button, a {
+        cursor: pointer;
+        transition: filter 0.2s;
+    }
+
+    button:hover, a:hover {
+        filter: brightness(0.9);
+    }
+
+`

32138654d87f0a9444b169d60bfdbf43cd37cf77
Yara Daudt <yaracrisalis@gmail.com>
2024-07-26 16:58:15 +0100

    Added theme colors

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
 import { Details } from './pages/Details'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Details />
-  </React.StrictMode>,
+    <ThemeProvider theme={theme}>
+      <Details />
+    </ThemeProvider>
+  </React.StrictMode>
 )
---
+++ src/styles/theme.js
@@ -0,0 +1,33 @@
+export default {
+    COLORS: {
+        BACKGROUND_100: "#000405",
+        BACKGROUND_200: "#00070A", 
+        BACKGROUND_300: "#000204", 
+        BACKGROUND_400: "#000A0F", 
+        BACKGROUND_500: "#000C12", 
+        BACKGROUND_600: "#00111A",
+        BACKGROUND_700: "#001119",
+        BACKGROUND_800: "#0D161B",
+        BACKGROUND_900: "#0D1D25",
+        BACKGROUND_1000: "#192227",
+
+        LIGHT_100: "#FFFFFF",
+        LIGHT_200: "#FFFAF1",
+        LIGHT_300: "#E1E1E6",
+        LIGHT_400: "#C4C4CC",
+        LIGHT_500: "#7C7C8A",
+        LIGHT_600: "#76797B",
+        LIGHT_700: "#4D585E",
+
+        RED_100: "#750310",
+        RED_200: "#92000E",
+        RED_300: "#AB222E",
+        RED_400: "#AB4D55",
+
+        BLUE_100: "#065E7C",
+        BLUE_200: "#82F3FF",
+
+        GRADIENT_100: "#091E26",
+        GRADIENT_200: "#00131C"
+    }
+}

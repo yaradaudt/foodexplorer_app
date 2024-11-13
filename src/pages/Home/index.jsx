1d68a4de698f194601b1286383122b3b232291cc
Yara Daudt <yaracrisalis@gmail.com>
2024-11-13 13:22:59 +0000

    Banner responsive

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -14,7 +14,7 @@ import bannerSVG from "../../assets/banner.svg";
 export function Home() {
   return (
     <Container>
-      <Header />
+
       <main>
         <Content>
           <Banner>
@@ -37,7 +37,7 @@ export function Home() {
           <Section categoryName="Bebidas" />
         </Content>
       </main>
-      <Footer />
+      
     </Container>
   );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { DEVICE_BREAKPOINTS } from "../../styles/deviceBreakpoins";
 
 export const Container = styled.div`
   width: 100%;
@@ -7,7 +8,6 @@ export const Container = styled.div`
   flex-direction: column;
 
   main {
-    grid-area: content;
     justify-self: center;
     overflow-y: scroll;
   }
@@ -21,11 +21,15 @@ export const Content = styled.div`
   left: 1;
   align-items: center;
   justify-content: center;
+
+  @media (max-width: ${DEVICE_BREAKPOINTS.SM}) {
+    margin: 8rem 0;
+  }
 `;
 export const Banner = styled.div`
   display: flex;
   position: relative;
-  width: 78rem;
+  width: 87%;
   height: 16.25rem;
   padding: 0 6rem;
   border-radius: 0.5rem;
@@ -34,12 +38,40 @@ export const Banner = styled.div`
     ${({ theme }) => theme.COLORS.GRADIENT_200} 0%,
     ${({ theme }) => theme.COLORS.GRADIENT_300} 100%
   );
+
+  @media (max-width: ${DEVICE_BREAKPOINTS.LG}) {
+    height: 14rem;
+  }
+
+  @media (max-width: ${DEVICE_BREAKPOINTS.MD}) {
+    height: 10rem;
+  }
+
+  @media (max-width: ${DEVICE_BREAKPOINTS.SM}) {
+    height: 6rem;
+  }
 `;
 export const BannerImage = styled.div`
   display: flex;
   position: absolute;
   left: -4.1rem;
   bottom: -0.86rem;
+  @media (max-width: ${DEVICE_BREAKPOINTS.SM}) {
+    left: -2.1rem;
+    bottom: -0.1rem;
+  }
+  img {
+    width: 100%;
+    @media (max-width: ${DEVICE_BREAKPOINTS.LG}) {
+      width: 80%;
+    }
+    @media (max-width: ${DEVICE_BREAKPOINTS.MD}) {
+      width: 50%;
+    }
+    @media (max-width: ${DEVICE_BREAKPOINTS.SM}) {
+      width: 40%;
+    }
+  }
 `;
 export const BannerText = styled.div`
   position: absolute;
@@ -48,16 +80,41 @@ export const BannerText = styled.div`
   top: 5.5rem;
   right: 6.25rem;
 
+  @media (max-width: ${DEVICE_BREAKPOINTS.LG}) {
+    top: 5rem;
+    right: 2rem;
+  }
+
+  @media (max-width: ${DEVICE_BREAKPOINTS.MD}) {
+    top: 3rem;
+    right: 1rem;
+  }
+
+  @media (max-width: ${DEVICE_BREAKPOINTS.SM}) {
+    top: 1.5rem;
+    right: 1rem;
+  }
+
   > h1 {
     font-family: "Poppins", sans-serif;
     font-weight: 500;
     font-size: 2.5rem;
     color: ${({ theme }) => theme.COLORS.LIGHT_300};
-  }
 
+    @media (max-width: ${DEVICE_BREAKPOINTS.MD}) {
+      font-size: 2rem;
+    }
+    @media (max-width: ${DEVICE_BREAKPOINTS.SM}) {
+      font-size: 1.5rem;
+    }
+  }
   > p {
     font-size: 1rem;
     font-weight: 400;
     color: ${({ theme }) => theme.COLORS.LIGHT_300};
+
+    @media (max-width: ${DEVICE_BREAKPOINTS.SM}) {
+      font-size: 0.7rem;
+    }
   }
 `;

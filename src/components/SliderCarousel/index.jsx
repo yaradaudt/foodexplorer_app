4297970deb225e1f61172ab30ee91c7fb9f41abf
Yara Daudt <yaracrisalis@gmail.com>
2024-11-21 16:12:55 +0000

    Slick slider warning fixed

--- src/components/SliderCarousel/index.jsx
+++ src/components/SliderCarousel/index.jsx
@@ -1,76 +1,81 @@
-import { Container, RightArrowWrapper, LeftArrowWrapper, CustomNextArrow, CustomPrevArrow } from './styles'
-import { DishCard } from '../../components/DishCard'
+import {
+  Container,
+  RightArrowWrapper,
+  LeftArrowWrapper,
+  CustomNextArrow,
+  CustomPrevArrow,
+} from "./styles";
+import { DishCard } from "../../components/DishCard";
 
-import Slider from  'react-slick'
+import Slider from "react-slick";
 
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export function SliderCarousel({ dishes }) {
-    const settings = {
-        dots: false,
-        infinite: true,
-        speed: 800,
-        slidesToShow: 3.5,
-        slidesToScroll: 1,
-        centerMode: true,
-        centerPadding: "4%",
-        focusOnSelect: false, 
-        swipeToSlide: true,
-        arrows: true,
-        nextArrow: (
-            <RightArrowWrapper>
-                <CustomNextArrow />
-            </RightArrowWrapper>
-        ),
-        prevArrow: (
-            <LeftArrowWrapper>
-                <CustomPrevArrow />
-            </LeftArrowWrapper>
-        ),
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                    centerMode: true,
-                    centerPadding: "2%",
-                    arrows: true,
-                },
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                    centerMode: true,
-                    centerPadding: "2%",
-                    arrows: false,
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 800,
+    slidesToShow: 3.5,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: "4%",
+    focusOnSelect: false,
+    swipeToSlide: true,
+    arrows: true,
+    customPaging: (i) => <div>{i + 1}</div>,
+    nextArrow: (
+      <RightArrowWrapper>
+        <CustomNextArrow />
+      </RightArrowWrapper>
+    ),
+    prevArrow: (
+      <LeftArrowWrapper>
+        <CustomPrevArrow />
+      </LeftArrowWrapper>
+    ),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          centerMode: true,
+          centerPadding: "2%",
+          arrows: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          centerMode: true,
+          centerPadding: "2%",
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 425,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          centerMode: false,
+          centerPadding: "0%",
+          arrows: false,
+        },
+      },
+    ],
+  };
 
-                },
-            },
-            {
-                breakpoint: 425,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                    centerMode: false,
-                    centerPadding: "0%",
-                    arrows: false,
-
-                },
-            },
-        ]
-    }
-
-    return(
-        <Container>
-            <Slider {...settings}>
-                {dishes.map((dish) => (
-                    <DishCard key={dish.id} data={dish}/>
-                ))}
-                </Slider>
-        </Container>
-    )
+  return (
+    <Container>
+      <Slider {...settings}>
+        {dishes.map((dish) => (
+          <DishCard key={dish.id} data={dish} />
+        ))}
+      </Slider>
+    </Container>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import GlobalStyles from './styles/global'
-import { ThemeProvider } from 'styled-components'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import GlobalStyles from "./styles/global";
+import { ThemeProvider } from "styled-components";
+import { StyleSheetManager } from "styled-components";
+import isPropValid from "@emotion/is-prop-valid";
 
-import { AuthProvider } from './hooks/auth'
-import { CartProvider } from './hooks/cart'
+import { AuthProvider } from "./hooks/auth";
+import { CartProvider } from "./hooks/cart";
 
-import { Routes } from './routes'
+import { Routes } from "./routes";
 
-import theme from './styles/theme'
-import { ToastContainer, toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import theme from "./styles/theme";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <CartProvider>
-        <AuthProvider>
-          <Routes />
-        </AuthProvider>
-      </CartProvider>
-    </ThemeProvider>
-    <ToastContainer position="top-center" autoClose={1000} hideProgressBar={true} theme='dark' toastStyle={{ 
-      backgroundColor: "#00111A", 
-      border: "2px solid #065E7C",
-    }}
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <StyleSheetManager shouldForwardProp={isPropValid}>
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <CartProvider>
+          <AuthProvider>
+            <Routes />
+          </AuthProvider>
+        </CartProvider>
+      </ThemeProvider>
+      <ToastContainer
+        position="top-center"
+        autoClose={1000}
+        hideProgressBar={true}
+        theme="dark"
+        toastStyle={{
+          backgroundColor: "#00111A",
+          border: "2px solid #065E7C",
+        }}
       />
-  </React.StrictMode>
-)
+    </React.StrictMode>
+  </StyleSheetManager>
+);
